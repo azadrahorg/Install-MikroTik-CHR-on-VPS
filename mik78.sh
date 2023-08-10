aecf013778b26e0861c393d697c25076dba566a3
azadrahorg <117533100+azadrahorg@users.noreply.github.com>
2023-08-10 06:06:48 -0400

    Update to 7.10.2

--- mik78.sh
+++ mik78.sh
@@ -6,18 +6,14 @@ echo "=== https://github.com/azadrahorg ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
-
-wget https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip -O chr.img.zip  && \
+apt update & apt install wget curl
+wget https://download.mikrotik.com/routeros/7.10.2/chr-7.10.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
 ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
-echo u > /proc/sysrq-trigger && \
-dd if=chr.img bs=1M of=/dev/$STORAGE iflag=fullblock && \
-echo "sync disk" && \
-echo s > /proc/sysrq-trigger && \
-echo "Sleep 5 seconds" && \
-sleep 5 && \
+dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
+echo 1 > /proc/sys/kernel/sysrq && \
+echo b > /proc/sysrq-trigger && \
 echo "Ok, reboot" && \
-echo b > /proc/sysrq-trigger
