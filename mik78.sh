bf9da6f226c6d45e26a85c94ac7b87c1b315b755
azadrahorg <117533100+azadrahorg@users.noreply.github.com>
2023-08-10 06:19:24 -0400

    Update to 7.10.2

--- mik78.sh
+++ mik78.sh
@@ -10,9 +10,13 @@ apt update & apt install wget curl
 wget https://download.mikrotik.com/routeros/7.10.2/chr-7.10.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
+echo STORAGE is $STORAGE && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
+echo ETH is $ETH && \
 ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
+echo ADDRESS is $ADDRESS && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo GATEWAY is $GATEWAY && \
 dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
 echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
