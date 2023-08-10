aba329572192af0129fbc2997d38ea4137d32eba
azadrahorg <117533100+azadrahorg@users.noreply.github.com>
2023-08-10 06:08:42 -0400

    Update to 7.10.2

--- mik78.sh
+++ mik78.sh
@@ -14,6 +14,6 @@ ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
 ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
+echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
 echo b > /proc/sysrq-trigger && \
-echo "Ok, reboot" && \
